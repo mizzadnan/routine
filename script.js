ee9fe40b9fde64080f11f2764b3468e75d74867b
mizzadnan <mirza.adnan2205@gmail.com>
2021-01-19 23:34:28 +0600

    wip

---
+++ script.js
@@ -0,0 +1,16 @@
+const time = document.querySelector("#time");
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let today = new Date();
+
+let day = days[today.getDay()];
+let month = months[today.getMonth()]
+let date = String(today.getDate())
+let year = String(today.getFullYear())
+
+time.textContent = `${day}, ${month} ${date}, ${year}`
+
+
+
